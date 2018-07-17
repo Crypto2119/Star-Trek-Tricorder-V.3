7d656019b48d364eadc2d579912732ea7cab176c
John <40526814+Crypto2119@users.noreply.github.com>
2018-07-16 21:17:10 -0700

    Added Main Files

---
+++ Star-Trek-Tricorder-03/Tricorder/Old Tricorders/Old Tests/SFX_Tricorder/SFX_Tricorder.ino
@@ -0,0 +1,19 @@
+
+const int buzzer = 8;//Defines The Buzzer
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);//Sets the Led as the Output
+  pinMode(buzzer, OUTPUT);//Sets the Buzzer as the Output
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);//Turns on the LED
+  tone(buzzer, 3000);//Rings the Buzzer at 3 Hertz
+  delay(10);//Waits 10 Millisecs
+  digitalWrite(LED_BUILTIN, LOW);//Turns Off The LED
+  noTone(buzzer);//Turns off the Buzzer
+  delay(30);//Waits 30 Millisecs
+}
+//Use This With Another Board Not Your Main Tricorder
+//This Will Be Put Together Into One Shell(Or Just Use a Larger Bread Board) 
---
+++ Star-Trek-Tricorder-03/Tricorder/Old Tricorders/Old Tests/Tricorder_10(visual_studio)/Tricorder_10(visual_studio).ino
@@ -0,0 +1,160 @@
+//=============================================================
+//            SETUP:                                          |
+//-------------------------------------------------------------
+//*THIS PROGRAM WAS INTENDED FOR USE WITH THE ARDUINO UNO R3* |
+//-------------------------------------------------------------
+//GO TO SKETCH AND DOWNLOAD THE LIBRARIES BELOW:              |
+//  1.DHT.H                                                   |
+//  2.LIQUIDCRYSTAL.H                                         |
+//IF IT DOES NOT WORK, THEN                                   |
+//TRY DOWNLOADING ALL OF THE LIRARIES                         |
+//-------------------------------------------------------------
+//=============================================================
+//SETUP THE CORRECT TOOLS:                                    |
+//- SET BOARD TO THE CORRECT BOARD                            |
+//- SET THE CORRECT BOARD (IF POSSIBLE)                       |
+//- SET THE UPLOAD SPEED TO 115200 OR YOUR OWN BOARD SPEED    |
+//=============================================================
+//=============================================================
+// DHT11 PINS:                                                |
+//      (PIN 1)Data: D7                                       |
+//      (PIN 2)VCC: 5V or 3V                                  |
+//      (PIN 3)GND: GND                                       |
+//=============================================================
+//---------------------------------------------------------------
+//CHECK THE SCHEMATIC FOR THE LCD DISPLAY IN THE ZIP FILE       |
+//---------------------------------------------------------------
+#include <LiquidCrystal.h>//LIBRARY FOR THE LCD DISPLAY
+#include "DHT.h"//LIBRARY FOR THE DHT11 (NAME OF THE HUMIDITY SENSOR)
+
+// set the DHT Pin
+#define DHTPIN 8//STATES POSITION OF THE DHT11 DATA PINS
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+
+  // BEGINS THE SETUP OF THE PROGRAM: 
+  lcd.begin(16, 2);//SETS THE LCD TO 16,2
+  dht.begin();//STARTS LCD
+  pinMode(LED_BUILTIN, OUTPUT);//defines the output
+  lcd.print("TRICORDER SETUP");//setup
+  delay((random(1000, 3000)));//WAITS 1000- 3000 MILLISECS (1-3 SECS)
+  lcd.setCursor(7,1);//RESETS THE CURSOR TO THE SECOND ROW
+  Serial.print("Setup.");
+  lcd.print(".");
+  delay((random(10, 30)));//WAITS 10-30 MILLISECS (0.01-0.03 SECS)
+  Serial.print("Setup..");
+  lcd.clear();//CLEARS LCD
+  lcd.print("..");
+  delay((random(10, 30)));//WAITS 10-30 MILLISECS (0.01-0.03 SECS)
+  Serial.print("Setup...");//LOADING
+  lcd.clear();//CLEAR LCD
+  lcd.print("...");//LOADING
+  delay((random(10, 30)));//WAITS 10-30 MILLISECS (0.01-0.03 SECS)
+  Serial.print("Setup.");
+  lcd.clear();//CLEAERS LCD
+  lcd.setCursor(7,1);//RESETS THE CURSOR TO THE SECOND ROW
+  lcd.print(".");
+  delay((random(10, 30)));//WAITS 10-30 MILLISECS (0.01-0.03 SECS)
+  Serial.print("Setup..");
+  lcd.clear();//CLEARS LCD
+  lcd.print("..");
+  delay((random(10, 30)));//WAITS 10-30 MILLISECS (0.01-0.03 SECS)
+  Serial.print("Setup...");
+  lcd.clear();//CLEARS LCD
+  lcd.print("...");
+  delay((random(10, 30)));//WAITS 10-30 MILLISECS (0.01-0.03 SECS)
+  Serial.print("Setup.");
+  lcd.clear();//CLEARS LCD
+  lcd.setCursor(7,1);//RESETS THE CURSOR TO THE SECOND ROW
+  lcd.print(".");
+  delay((random(10, 30)));//WAITS 10-30 MILLISECS (0.01-0.03 SECS)
+  Serial.print("Setup..");
+  lcd.clear();//CLEARS LCD
+  lcd.print("..");
+  delay((random(10, 30)));//WAITS 10-30 MILLISECS (0.01-0.03 SECS)
+  Serial.print("Setup...");
+  lcd.clear();//CLEARS LCD
+  lcd.print("...");
+  delay((random(10, 3000)));//WAITS 10-3000 MILLISECS (0.01-3 SECS)
+  Serial.print("Setup.");
+  lcd.clear();//CLEARS LCD
+  pinMode(LED_BUILTIN, OUTPUT);//defines the output
+ }
+ 
+void loop() {//BEGINS MAIN PART OF THE PROGRAM
+  // PRINT DATA TO THE LCD.
+  lcd.print("Temp(F) |  Humidity|");
+  delay((random(10, 30)));//waits 10-30 millisecs (0.01-0.03)
+  Serial.print("Temp:");
+  digitalWrite(LED_BUILTIN, HIGH);//turns on the led
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1); //RESETS CURSOR TO THE FIRST ROW 
+  // read humidity
+  float h = dht.readHumidity();//SETS UP THE DHT11 IN HUMIDITY
+  //read temperature in Fahrenheit
+  float f = dht.readTemperature(true);//TESTS THE DHT11 IN TEMPERATURE (F)
+  float c = dht.readTemperature(true);//TEST THE DHT11 IN TEMPERATURE (C)
+
+  delay((random(10, 30)));//waits 10-30 millisecs (0.01-0.03)
+  digitalWrite(LED_BUILTIN, LOW);//turns off the led
+  if (isnan(h) || isnan(f)) {//if there is an error, the error message will show on screen
+    lcd.print("|==| TRICORDER ERROR |==|");//error message
+    Serial.print("TRICORDER ERROR");
+    delay(1000);//WAIT 1000 MILLISECS (1 SECS)
+    lcd.print("REALIGN DHT11 PINS");//TROUBLESHOOTING TIPS
+    delay(1000);//WAIT 1000 MILLISECS (1 SECS)
+    lcd.print("CHECK PINS");//TROUBLESHOOTING TIPS
+    return;//RESETS THE CURSOR AND CLEARS THE LCD
+  }
+  lcd.print(f);//PRINTS THE TERMPERATURE IN F
+  Serial.print(f);//PRINTS THE F IN SERIAL MONITER
+  lcd.setCursor(7,1);//RESETS THE CURSOR TO THE SECOND ROW
+  lcd.print(h); //PRINTS THE HUMIDITY
+  Serial.print("HUMIDITY");//WRITES THE LABEL FOR THE HUMIDITY ON THE SERIAL MONITER
+  Serial.print(h);//PRINTS THE HUMIDITY IN THE SERIAL MONITER
+  delay((random(900, 1000))); digitalWrite(LED_BUILTIN, HIGH);//WAITS 900-1000 MILLISECS(0.9-1 SECS)AND TURNS ON THE LED
+  lcd.clear();//CLEARS LCD
+  //UNCOMMENT THESE TO USE CELSIUS
+//  lcd.print("CELSIUS: ");//WRITES TEXT
+//  (7,1);//RESETS CURSOR TO THE SECOND ROW
+//  lcd.print(5/9*(f-32));//CONVERTS TEMPERATURE F TO C WITH THE CONVERSION EQUATION (5/9*(F-32) USES ACII CHARS)
+//  delay((random(900, 1000))); digitalWrite(LED_BUILTIN, LOW);//WAITS 900-1000 MILLISECS(0.9-1 SECS)AND TURNS OFF THE LED
+//  lcd.clear();//CLEARS LCD  
+}
+//OBJECTIVE:
+//THE OBJECTIVE OF THIS PROJECT IS TO CREATE A STAR TREK TRICORDER AS REAL AS POSSIBLE.
+//THIS ALSO MEANS THAT IT MUST SHOW REAL DATA IN THE SURROUNDINGS, OR IT WILL WORK AS A PROP.
+//*USE A CASE THAT LOOKS LIKE THE REAL THING*
+//CHANGELOG:
+//VERSION 1
+//7/1/18
+//CREATED PROGRAM
+//MADE SCHEMATICS
+//7/2/18
+//MADE PROGRAM BASE AND UPLOADED FOR TEST
+//ADDED TEMPERATURE F TO C EQUATION FOR MULTI-CONTINENT USE(USES METRIC AND IMPERIAL UNITS)
+//VERSION 9
+//7/3/18
+//ADDED INCLUDED DEBUGGING SERVICING FOR THE DHT11(STILL WORKING ON THE LCD)
+//ADDED MORE COMMENTS FOR LEARNERS
+//NEEDED FIXES SO FAR:
+// - F-C CONVERSION BUG
+// - CHECK COMMENTS FOR ACCURACY
+// - DELETE ANY UNNECCESSARY LINES
+//VERSION 10
+//7/5/18
+//REMOVED THE CELSIUS FUNCTION TO FIX THE BUG
+//ADDED SERIAL PORT USE INSTEAD OF THE DISPLAY
+//MORE COMMENTING
+
+//               NOTES:
+//-WORKS MOSTLY WITH ARDUINO GENUINO OR UNO
+//-SET MAINLY FOR FAREHNHEIT, REQUIRES CONFIGURATION FOR CELCIUS
+//-UNPLUG THE CIRCUIT IF IT BECOMES HOT AND CHECK THE PINS FOR WRONG PLUG IN
+//-DHT11 QUICKLY GETS HEATED ON A SHORT CIRCUIT
